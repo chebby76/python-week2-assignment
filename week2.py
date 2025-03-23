966c67330e8126a5453eab248de06fd492abb7ae
chebby76 <vchebby876@gmail.com>
2025-03-23 17:46:01 +0300

    Add files via upload

---
+++ week2.py
@@ -0,0 +1,18 @@
+#creating my_list empty list
+my_list=[]
+#appending items to my list
+my_list=[10,20,30,40]
+#inserting 15 in my list
+my_list.insert(1,15)
+list2=[50,60,70]
+#extending my list
+my_list.extend(list2)
+print(my_list)
+#removing 70 fron my list
+my_list.remove(70)
+print(my_list)
+#sorting my list
+my_list.sort()
+print (my_list)
+#printing 30
+print(my_list[3])
